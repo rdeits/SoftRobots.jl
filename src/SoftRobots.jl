c1bfee64293526377f2c973a55e93ec9977ae753
Robin Deits <robin.deits@gmail.com>
2016-03-16 13:25:13 -0400

    working single-object sim in 3d with LCMGL

--- src/SoftRobots.jl
+++ src/SoftRobots.jl
@@ -4,9 +4,15 @@ module SoftRobots
 
 using SpatialFields
 import MultiPoly: MPoly
-import PyCall
-# import PyPlot
+using GeometryTypes
 import DataStructures: OrderedDict
+using PyCall
+global spatial
+using LCMGL
+
+function __init__()
+    global spatial = pyimport("scipy.spatial")
+end
 
 abstract Object
 abstract StaticObject <: Object
@@ -24,15 +30,10 @@ type PointMass
     mass::Float64
 end
 
-type Spine
-    parent::Int
-    child::Int
-end
-
 type SoftRobot <: DynamicObject
     nodes::Vector{PointMass}
     edges::Vector{DampedSpring}
-    spines::Vector{Spine}
+    faces::Vector{Face{3}}
 end
 
 immutable FixedObject <: StaticObject
@@ -46,10 +47,6 @@ type World
     World(gravity::Vector) = new([], gravity, 1.0)
 end
 
-function World2D()
-    World([0, -9.81])
-end
-
 function World3D()
     World([0, 0, -9.81])
 end
@@ -61,8 +58,8 @@ abstract StaticObjectState <: ObjectState
 typealias WorldState Dict{Object, ObjectState}
 
 type SoftRobotState{T} <: DynamicObjectState
-    positions::Vector{Vector{T}}
-    velocities::Vector{Vector{T}}
+    positions::Vector{Point{3, T}}
+    velocities::Vector{Point{3, T}}
     barrier::ScalarField
     velocity_field::VectorField
 
@@ -70,14 +67,18 @@ type SoftRobotState{T} <: DynamicObjectState
 end
 
 function SoftRobotState{T}(positions::Vector{Vector{T}}, velocities::Vector{Vector{T}})
-	SoftRobotState{T}(positions, velocities)
+	SoftRobotState{T}(map(x -> convert(Point{3, T}, x), positions), map(x -> convert(Point{3, T}, x), velocities))
+end
+
+function SoftRobotState{T}(positions::Vector{Point{3, T}}, velocities::Vector{Point{3, T}})
+    SoftRobotState{T}(map(x -> convert(Point{3, T}, x), positions), map(x -> convert(Point{3, T}, x), velocities))
 end
 
 type FixedObjectState <: StaticObjectState
     barrier::ScalarField
     velocity_field::VectorField
 
-    FixedObjectState(barrier) = new(barrier, [MPoly{Float64}(OrderedDict(zeros(Int64, length(barrier.vars)) => 0.0), barrier.vars), MPoly{Float64}(OrderedDict(zeros(Int64, length(barrier.vars)) => 0.0), barrier.vars)])
+    FixedObjectState(barrier) = new(barrier, [MPoly{Float64}(OrderedDict(zeros(Int64, length(barrier.vars)) => 0.0), barrier.vars) for i in 1:length(barrier.vars)])
 end
 
 function update!(world::World, states::Dict{Object, ObjectState}, dt::Number)
@@ -117,19 +118,19 @@ function normalize!(x)
 end
 
 function update_barrier!(robot::SoftRobot, state::SoftRobotState)
-    points = hcat([state.positions[s.parent] for s in robot.spines]...)
-    normals = hcat([state.positions[s.parent] - state.positions[s.child] for s in robot.spines]...)
-    for i = 1:size(normals, 1)
-        normalize!(normals[i,:])
-    end
-    state.barrier = HermiteRadialField(points, normals)
+    # points = hcat([state.positions[s.parent] for s in robot.spines]...)
+    # normals = hcat([state.positions[s.parent] - state.positions[s.child] for s in robot.spines]...)
+    # for i = 1:size(normals, 1)
+    #     normalize!(normals[i,:])
+    # end
+    # state.barrier = HermiteRadialField(points, normals)
 end
 
 function update_velocity_field!{T}(robot::SoftRobot, state::SoftRobotState{T})
     mean_velocity = mean(state.velocities)
     # TODO: linear regression on velocity as a function of [x, y]
     dimension = length(state.positions[1])
-    state.velocity_field = PolynomialVectorField([MPoly{T}(OrderedDict([0, 0] => v), [:x, :y, :z][1:dimension]) for v in mean_velocity])
+    state.velocity_field = PolynomialVectorField([MPoly{T}(OrderedDict(zeros(dimension) => v), [:x, :y, :z][1:dimension]) for v in mean_velocity])
 end
 
 function collisions!(robot::SoftRobot, states::Dict{Object, ObjectState}, friction::Number, dt::Number)
@@ -155,30 +156,32 @@ function rotmat(theta)
      sin(theta) cos(theta)]
 end
 
-function barrier_collisions!(robot::SoftRobot, my_state::SoftRobotState, other_state::ObjectState, friction::Number, dt::Number)
+function barrier_collisions!{T}(robot::SoftRobot, my_state::SoftRobotState{T}, other_state::ObjectState, friction::Number, dt::Number)
 	barrier_graident = grad(other_state.barrier)
     for i = 1:length(robot.nodes)
-        relative_velocity = my_state.velocities[i] - evaluate(other_state.velocity_field, my_state.positions[i])
+        barrier_velocity = Point(evaluate(other_state.velocity_field, my_state.positions[i])...)
+        relative_velocity = my_state.velocities[i] - barrier_velocity
         predicted_relative_position = my_state.positions[i] + relative_velocity * dt
         if evaluate(other_state.barrier, predicted_relative_position) <= 0
             # println("collision with barrier: " , other_state.barrier)
             # @show my_state.velocities[i]
             # @show my_state.positions[i]
             # @show relative_velocity
-            # @show predicted_relative_position
-            # @show value(other_state.barrier, predicted_relative_position...)
-            gradient = evaluate(barrier_graident, predicted_relative_position)
+            gradient = Point(evaluate(barrier_graident, predicted_relative_position))
             normal = gradient / norm(gradient)
             penetration_velocity = -dot(relative_velocity, normal)
+            # @show penetration_velocity
             if penetration_velocity > 0
-                my_state.velocities[i] += normal * penetration_velocity
+                # my_state.velocities[i] += normal * penetration_velocity
+                my_state.velocities[i] = barrier_velocity
             end
-            gradient = rotmat(friction * pi*(rand(1) - 0.5)) * gradient
-            normal = gradient / norm(gradient)
-            penetration_velocity = -dot(relative_velocity, normal)
-            if penetration_velocity > 0
-                my_state.velocities[i] += normal * penetration_velocity
-            end
+            # @show my_state.velocities[i]
+            # gradient = rotmat(friction * pi*(rand(1) - 0.5)) * gradient
+            # normal = gradient / norm(gradient)
+            # penetration_velocity = -dot(relative_velocity, normal)
+            # if penetration_velocity > 0
+            #     my_state.velocities[i] += normal * penetration_velocity
+            # end
         end
     end
 end
@@ -213,37 +216,89 @@ end
 function euler_integrate!(robot::SoftRobot, state::SoftRobotState, total_forces, dt::Real)
     dim = length(state.positions[1])
     for i = 1:length(robot.nodes)
-        for j = 1:dim
-            state.positions[i][j] += state.velocities[i][j] * dt
-            state.velocities[i][j] += total_forces[i][j] / robot.nodes[i].mass * dt
-        end
+        half_delta_v = Point(total_forces[i]) / robot.nodes[i].mass * dt / 2.0
+        state.velocities[i] += half_delta_v
+        # state.velocities[i] += Point(total_forces[i]) / robot.nodes[i].mass * dt
+        state.positions[i] += state.velocities[i] * dt
+        state.velocities[i] += half_delta_v
     end
 end
 
-function ring()
-    nodes = [PointMass(0.1) for i in 1:10]
-    edges = []
-    for j = 1:10
-        for k = j+1:10
-            push!(edges, [j,k])
-        end
-    end
-    # edges = Any[[i, i+1] for i in 1:9]
-    # push!(edges, [10,1])
-    edges = [DampedSpring(e[1], e[2], 2000.0, 4.0, 0.05) for e in edges]
-    gravity = [0, -9.81]
-    r = SoftRobot(nodes, edges)
+# function ring()
+#     nodes = [PointMass(0.1) for i in 1:10]
+#     edges = []
+#     for j = 1:10
+#         for k = j+1:10
+#             push!(edges, [j,k])
+#         end
+#     end
+#     # edges = Any[[i, i+1] for i in 1:9]
+#     # push!(edges, [10,1])
+#     edges = [DampedSpring(e[1], e[2], 2000.0, 4.0, 0.05) for e in edges]
+#     gravity = [0, -9.81]
+#     r = SoftRobot(nodes, edges)
 
-    positions = [[i/40 + 0.75; 0.8 + rand(1)*0.01 + 0.05 * mod(i,2)] for (i,n) in enumerate(r.nodes)]
-    velocities = [zeros(2) for n in r.nodes]
-    state = SoftRobotState(positions, velocities)
+#     positions = [[i/40 + 0.75; 0.8 + rand(1)*0.01 + 0.05 * mod(i,2)] for (i,n) in enumerate(r.nodes)]
+#     velocities = [zeros(2) for n in r.nodes]
+#     state = SoftRobotState(positions, velocities)
 
-    r, state
-end
+#     r, state
+# end
 
 #   2 5 8
 # 1 3 6 9
 #   4 7 10
+
+function convex_hull(nodes)
+    hull = spatial[:ConvexHull](hcat(map(x -> convert(Vector, x), nodes)...)')
+    simplices = hull[:simplices]
+    return [Face((simplices[i,:]+1)...) for i in 1:size(simplices, 1)]
+end
+
+function tetrahedron()
+    # k = 4000.0
+    k = 4000.0
+    b = 4.0
+    l = 0.05
+    m = 0.1
+
+    nodes = [PointMass(m) for i in 1:4]
+    edges = [DampedSpring(x, y, k, b, l) for (x, y) in Any[[1,2], [1,3], [1,4], [2,3], [2,4], [3, 4]]]
+    positions = [Point(0., 0, 0.2), Point(0.05, 0, 0.2), Point(0., 0.05, 0.2), Point(0.02, 0.02, 0.25)]
+    faces = convex_hull(positions)
+    robot = SoftRobot(nodes, edges, faces)
+    velocities = [Point{3, Float64}(0) for n in robot.nodes]
+    state = SoftRobotState(positions, velocities)
+    robot, state
+end
+
+function blob(;k=4000., b=4.0, m=0.1, num_nodes=20)
+    nodes = [PointMass(m) for i in 1:num_nodes]
+    positions = Point{3, Float64}[rand(3)+[0.; 0; 0.1] for i in 1:num_nodes]
+    edges = DampedSpring[]
+    for i = 1:num_nodes
+        for j = (i+1):num_nodes
+            rest_length = norm(positions[i] - positions[j])
+            push!(edges, DampedSpring(i, j, k, b, rest_length))
+        end
+        # for k = 1:edges_per_node
+        #     connected_nodes = []
+        #     j = rand(1:num_nodes)
+        #     while j == i || j in connected_nodes
+        #         j = rand(1:num_nodes)
+        #     end
+        #     push!(connected_nodes, j)
+        #     rest_length = norm(positions[i] - positions[j])
+        #     push!(edges, DampedSpring(i, j, k, b, rest_length))
+        # end
+    end
+
+    faces = convex_hull(positions)
+    robot = SoftRobot(nodes, edges, faces)
+    velocities = [Point{3, Float64}(0) for n in robot.nodes]
+    state = SoftRobotState(positions, velocities)
+    robot, state
+end
 
 function snake()
     k = 4000.0
@@ -254,20 +309,17 @@ function snake()
     edges = [DampedSpring(x, y, k, b, l) for (x, y) in Any[[1,2], [1,3], [1,4]]]
     x = 0.4
     y = 0.8
-    positions = Array{Float64, 1}[[x, y]]
-    spines = [Spine(1, 3)]
+    positions = Point{3, Float64}[[x, y, 0]]
     rows = 8
     for row = 1:8
         x += l
         for i = 1:3
             push!(nodes, PointMass(m))
-            push!(positions, [x, y + (i - 2) * l])
+            push!(positions, Point(x, y + (i - 2) * l, 0))
         end
         for i = 1:2
             push!(edges, DampedSpring(1 + i + (row-1) * 3, i + 2 + (row-1) * 3, k, b, l))
         end
-        push!(spines, Spine(2 + (row-1) * 3, 3 + (row-1) * 3))
-        push!(spines, Spine(4 + (row-1) * 3, 3 + (row-1) * 3))
         if row != rows
             for i = 1:3
                 push!(edges, DampedSpring(1 + i + (row-1) * 3, 1 + i + (row) * 3, k, b, l))
@@ -280,26 +332,53 @@ function snake()
     end
     x += l
     push!(nodes, PointMass(m))
-    push!(positions, [x, y])
-    push!(spines, Spine(length(nodes), length(nodes) - 2))
+    push!(positions, Point(x, y, 0))
     for i = length(nodes)-3:length(nodes)-1
         push!(edges, DampedSpring(i, length(nodes), k, b, l))
     end
 
+    faces = convex_hull(positions)
+    robot = SoftRobot(nodes, edges, faces)
 
-
-    # edges = Any[[1,2], [1,3], [2,3], [4,5], [6,7], [8,9],
-    #             [8,10], [9,10], [2,4], [3,5], [4,6],
-    #     [5,7], [6,8], [7,9], [3,4], [5,6], [7,8],
-    #     [2,5], [4,7], [6,9]]
-    # edges = [DampedSpring(e[1], e[2], 2000.0, 4.0, 0.05) for e in edges]
-    r = SoftRobot(nodes, edges, spines)
-
-    # positions = [[i/40 + 0.6; 0.8 + rand(1)*0.01 + 0.05 * mod(i,2)] for (i,n) in enumerate(r.nodes)]
-    velocities = [zeros(2) for n in r.nodes]
+    velocities = [Point{3, Float64}(0) for n in r.nodes]
     state = SoftRobotState(positions, velocities)
-    r, state
+    robot, state
 end
+
+function mesh{T}(robot::SoftRobot, state::SoftRobotState{T})
+    HomogenousMesh(state.positions, robot.faces)
+end
+
+function draw(mesh::AbstractMesh)
+    LCMGLClient("soft_robot") do lcmgl
+        color(lcmgl, .7, .7, .2, 0.4)
+        begin_mode(lcmgl, LCMGL.TRIANGLES)
+        verts = vertices(mesh)
+        for face in faces(mesh)
+            for point in verts[face][end:-1:1]
+                vertex(lcmgl, point[1], point[2], point[3])
+            end
+        end
+        end_mode(lcmgl)
+        color(lcmgl, 0., 0., 0., 1.0)
+        begin_mode(lcmgl, LCMGL.LINES)
+        verts = vertices(mesh)
+        for face in faces(mesh)
+            for i = 1:length(verts[face])
+                vertex(lcmgl, verts[face][i]...)
+                j = i + 1
+                if i == length(verts[face])
+                    j = 1
+                end
+                vertex(lcmgl, verts[face][j]...)
+            end
+        end
+        end_mode(lcmgl)
+        switch_buffer(lcmgl)
+    end
+end
+
+draw(robot::SoftRobot, state::SoftRobotState) = draw(mesh(robot, state))
 
 # include("pyplot_visualizer.jl")
 
--- test/test_snake.jl
+++ test/test_snake.jl
@@ -1,27 +1,28 @@
 using SoftRobots
 using MultiPoly
-using ProfileView
+# using ProfileView
 
 function test_snake()
-    world = SoftRobots.World2D()
+    world = SoftRobots.World3D()
     world_state = Dict{SoftRobots.Object, SoftRobots.ObjectState}()
 
-    r, snake_state = SoftRobots.snake()
-    push!(world.objects, r)
-    world_state[r] = snake_state
+    robot, robot_state = SoftRobots.tetrahedron()
+    push!(world.objects, robot)
+    world_state[robot] = robot_state
 
-    x, y = generators(MPoly{Float64}, :x, :y)
+    x, y, z = generators(MPoly{Float64}, :x, :y, :z)
     terrain = SoftRobots.FixedObject()
-    terrain_state = SoftRobots.FixedObjectState(-(0.1 + 0.5x^2 - 1y))
+    terrain_state = SoftRobots.FixedObjectState(-(0.1 + 0.5x^2  + 0.5y^2 - 1z))
     push!(world.objects, terrain)
     world_state[terrain] = terrain_state
 
-    Profile.clear()
+    # Profile.clear()
     SoftRobots.update!(world, world_state, 0.001)
     @time @profile for j = 1:1000
         SoftRobots.update!(world, world_state, 0.001)
+        SoftRobots.draw(robot, world_state[robot])
     end
-    ProfileView.view()
+    # ProfileView.view()
 end
 
 test_snake()
