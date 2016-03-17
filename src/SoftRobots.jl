b8c23d2c592116b742a1cac2b55292f0f0656a2f
Robin Deits <robin.deits@gmail.com>
2016-03-17 15:49:19 -0400

    fix normal orientation and barrier drawing

--- src/SoftRobots.jl
+++ src/SoftRobots.jl
@@ -254,7 +254,29 @@ end
 function convex_hull{T}(nodes::Vector{Point{3, T}})
     hull = spatial[:ConvexHull](hcat(map(x -> convert(Vector, x), nodes)...)')
     simplices = hull[:simplices]
-    return Face{3, Int, 0}[vec(simplices[i,:]+1) for i in 1:size(simplices, 1)]
+    simplices += 1
+
+    faces = Face{3, Int, 0}[]
+    # Reorient simplices so that normals always point out from the hull
+    for i = 1:size(simplices, 1)
+        v = Vector{T}[nodes[simplices[i, j]] for j in 1:size(simplices, 2)]
+
+        # Given a face of a convex hull, all of the points in the body must be on the same side of that face. So in theory, we just need to pick one point not on the face and check which side it's on. But to attempt to be robust to numerical issues, we'll actually sum the dot product with the normal for every point in the body, and check whether that sum is positive or negative. If this becomes a performance bottleneck, we can revisit it later.
+        n = Point(cross(v[2] - v[1], v[3] - v[1]))
+        b = dot(n, nodes[simplices[i, 1]])
+        total = zero(T)
+        for j = 1:length(nodes)
+            total += dot(n, nodes[j]) - b
+        end
+        if total < 0
+            # Then the normal is pointing the right way
+            push!(faces, Face{3, Int, 0}((simplices[i,:])...))
+        else
+            # Otherwise the normal is pointing the wrong way, so we flip the face
+            push!(faces, Face{3, Int, 0}((simplices[i,end:-1:1])...))
+        end
+    end
+    return faces
 end
 
 function tetrahedron()
@@ -399,10 +421,24 @@ end
 function draw(lcmgl::LCMGLClient, robot::SoftRobot, state::SoftRobotState)
   body_mesh = HomogenousMesh(state.positions, robot.faces)
   draw(lcmgl, body_mesh)
-
-  # boundary_mesh = HomogenousMesh(SignedDistanceField(x -> evaluate(state.barrier, x), HyperRectangle(Vec(0., 0., 0.), Vec(4., 4, 4))))
-  # draw(lcmgl, boundary_mesh, face_color=[.8, .1, .1, .3])
   switch_buffer(lcmgl)
+
+  LCMGLClient("barrier$(lcmgl.pointer)") do lcmgl
+    center = Vec(mean(state.positions))
+    widths = Vec(1.5*(maximum(state.positions) - minimum(state.positions)))
+    lb = Vec(center - 0.5*widths)
+    ub = Vec(center + 0.5*widths)
+    bounds = HyperRectangle(lb, widths)
+
+    barrier = state.barrier
+    barrier_field = SignedDistanceField(x -> SoftRobots.evaluate(barrier, x), bounds, maximum(widths)/10);
+    barrier_mesh = HomogenousMesh(barrier_field, 0.0)
+    rescaled_points = Point{3,Float64}[Vec(v-1) ./ (Vec(size(barrier_field))-1) .* (ub - lb) + lb for v in vertices(barrier_mesh)]
+    barrier_mesh = HomogenousMesh(rescaled_points, barrier_mesh.faces)
+
+    draw(lcmgl, barrier_mesh, face_color=[.8, .1, .1, .3])
+    switch_buffer(lcmgl)
+  end
 end
 
 function copy_dict_values(dict::Dict)
